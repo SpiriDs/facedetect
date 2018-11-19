0639b6daa08815635a2f50dde34ed7e3cd3907e3
SpiriDs <spirids@live.de>
2018-11-19 11:55:41 +0700

    Change Navigation when signed in

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,6 @@ const Navigation = ({ onRouteChange, signedIn }) => {
             return(
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
                 <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-                <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
             </nav> 
             ) 
         } else {
